85fa003114570c2f69402a18fa45f2a013750f4e
lijinhong <lijinhong@icloud.com>
2023-11-21 23:03:13 +0800

    update

---
+++ git-push.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+python3.10 deal_img.py
+python3.10 deal_only_drive.py
+git rm -r --cached .
+git add .gitignore
+git commit -m 'update ignore'
+git add .
+git commit -m 'update'
+git push
