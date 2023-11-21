4fc0a5f6da14554e3c266cb7fd49c9efbc8e80bf
lijinhong <lijinhong@icloud.com>
2023-11-21 22:26:39 +0800

    update

--- deal_only_drive.py
+++ deal_only_drive.py
@@ -12,20 +12,19 @@ def filter_md_files(folder_path):
                 # 判断是否包含"onlyOneDrive: true"
                 if "onlyOneDrive: true" in content or "onlyOneDrive: \"true\"" in content or "onlyOneDrive: \"True\"" in content or "onlyOneDrive: \"TRUE\"" in content:
                     # 遍历.gitignore文件
-                    for root2, dirs2, files2 in os.walk(folder_path):
-                        for file_name2 in files2:
-                            if file_name2 == '.gitignore':
-                                gitignore_path = os.path.join(root2, file_name2)
-                                # 读取.gitignore文件内容
-                                with open(gitignore_path, 'r') as gitignore_file:
-                                    gitignore_content = gitignore_file.read()
-                                # 判断是否已经存在相同内容
-                                add_ignore_file_path = os.path.relpath(file_path, folder_path)
-                                if add_ignore_file_path in gitignore_content:
-                                    continue
-                                # 添加内容到.gitignore文件末尾
-                                with open(gitignore_path, 'a') as gitignore_file:
-                                    gitignore_file.write("\n" + add_ignore_file_path + "\n")
+                    gitignore_path = os.path.join(folder_path, '.gitignore')
+                    # 读取.gitignore文件内容
+                    with open(gitignore_path, 'r') as gitignore_file:
+                        gitignore_content = gitignore_file.read()
+                    # 判断是否已经存在相同内容
+                    add_ignore_file_path = os.path.relpath(file_path, folder_path)
+                    print('add_ignore_file_path', add_ignore_file_path, add_ignore_file_path in gitignore_content)
+                    if add_ignore_file_path in gitignore_content:
+                        continue
+                    # 添加内容到.gitignore文件末尾
+                    with open(gitignore_path, 'a') as gitignore_file:
+                        gitignore_file.write("\n" + add_ignore_file_path + " # onlyOneDrive \n")
+                        print('add_ignore_file_path', add_ignore_file_path)
 # 获取当前执行文件的路径（绝对路径）
 current_path = os.path.abspath(__file__)
 print("当前执行文件的路径：", current_path)
