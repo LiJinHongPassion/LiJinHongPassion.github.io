04a88e9732ad2e1eeca99908e4d56044cfb77673
lijinhong <lijinhong@icloud.com>
2024-07-20 16:39:32 +0800

    update

---
+++ deal_img.py
@@ -0,0 +1,79 @@
+import os
+import base64
+
+
+# 将md文件中的![[图片]]转化为base64的脚本
+
+
+def get_all_file_paths(directory_path):
+    file_paths = []
+    for root, dirs, files in os.walk(directory_path):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            file_paths.append(file_path)
+    return file_paths
+def get_paths_with_suffix(paths, suffix):
+    matching_paths = [path for path in paths if path.endswith(suffix)]
+    return matching_paths
+
+def replace_images_with_base64(directory_path):
+    # 获取指定目录下的所有文件
+    file_list = get_all_file_paths(directory_path)
+    # 遍历每个文件
+    for file_name in file_list:
+        if file_name.endswith(".md"):
+            
+            file_path = os.path.join(directory_path, file_name)
+            
+
+            #print('file_path:', file_path)
+            
+            # 读取md文件内容
+            with open(file_path, "r", encoding="utf-8") as file:
+                md_content = file.read()
+            # if '项目接入' in file_name:
+            #     print(md_content)
+
+            # 搜索图片文件名并替换为base64字符串
+            search_start_idx = 0
+            while True:
+                # 查找图片文件名的开始和结束位置
+                start_idx = md_content.find("![[", search_start_idx)
+                if start_idx == -1:
+                    break
+                end_idx = md_content.find("]]", start_idx)
+                if end_idx == -1:
+                    break
+
+                # 获取图片文件名
+                image_file_name = md_content[start_idx+3:end_idx]
+                print('image_name:', image_file_name)
+
+                # 在指定目录中查找图片文件并转换为base64字符串
+                image_path = get_paths_with_suffix(file_list, image_file_name)[0]
+                print('image_path:', image_path)
+                if os.path.exists(image_path):
+                    with open(image_path, "rb") as image_file:
+                        image_data = image_file.read()
+                    
+                    # 将图片文件替换为base64字符串
+                    image_base64 = base64.b64encode(image_data).decode("utf-8")
+                    md_content = md_content[:start_idx] + "![](data:image/png;base64," + image_base64 + ")" + md_content[end_idx+2:]
+                    os.remove(image_path)
+                search_start_idx = end_idx + 2
+
+            # 将替换后的内容写回md文件
+            with open(file_path, "w", encoding="utf-8") as file:
+                file.write(md_content)
+                
+
+
+print('deal')
+
+# 获取当前执行文件的路径（绝对路径）
+current_path = os.path.abspath(__file__)
+print("当前执行文件的路径：", current_path)
+
+# 获取当前执行文件所在目录的路径
+current_directory = os.path.dirname(current_path)
+replace_images_with_base64(current_directory)
